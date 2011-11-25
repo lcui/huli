0df747f7e97bc58c3c8ba8137059102b88d1b99d
unknown <lcui@LCUI-LT.nvidia.com>
2011-11-25 11:47:28 +0900

    added my vimrc.

---
+++ ji.py
@@ -0,0 +1,152 @@
+"""
+A simple wrapper for git commands.
+usage: python ji.py options cmds
+cmds should follow options
+    options:
+        -v                      > show git command to run
+    cmds:
+        cat file                > show file contents in the server
+        backup outdir id1, id2  > backup modified files
+        changelist              > list files you added/deleted/modified.
+"""
+
+import os, sys
+import shutil   #for copy
+
+verbose = False
+
+def ji(cmdline) :
+    if verbose:
+        print "\t-> Runing command: %s" % cmdline
+
+    return os.popen(cmdline).readlines()
+
+def jicat(fin, fout):
+    fin = os.path.abspath(fin)
+    if not os.path.exists(fin) :
+        print "%s does not exist" % fin
+        return
+
+    gitroot = get_gitroot(fin)
+    fin = fin.replace(gitroot, "")[1:]
+
+    fin = fin.replace('\\', '/')    # hack for DOS
+    cmdline = "git show HEAD:%s > %s" % (fin, fout)
+    ji(cmdline)
+
+def get_gitroot(fin):
+    rootdir = "/"
+    fin = os.path.abspath(fin)
+    if os.path.isdir(fin):
+        fin_dir = fin
+    else :
+        fin_dir = os.path.dirname(os.path.abspath(fin))
+    os.chdir(fin_dir)
+    try:
+        while fin_dir != rootdir:
+            #print fin_dir
+            if os.path.isdir(os.path.join(fin_dir, ".git")):
+                break
+            fin_dir = os.path.abspath(os.path.join(fin_dir, "../"))
+    except:
+        print "not in a git repo"
+        fin_dir = None
+    else:
+        fin_dir = os.path.abspath(fin_dir)
+        #print "find git root dir: " + fin_dir
+
+    return fin_dir
+
+#args format is: outdir  [ID1]  [ID2]
+def jibackup(argv, useless):
+    outdir = None
+    id1 = None
+    id2 = None
+    cmdline = "git diff --name-only "
+
+    if len(argv) == 1:
+        outdir = argv.pop(0)
+    elif len(argv) == 2:
+        outdir = argv.pop(0)
+        cmdline = cmdline + argv.pop(0)
+    elif len(argv) == 3:
+        outdir = argv.pop(0)
+        cmdline = cmdline + argv.pop(0) + " " + argv.pop(0)
+
+    if outdir :
+        outdir = os.path.abspath(outdir)
+    else:
+        outdir = "c:/tmp"
+
+    print outdir
+    print cmdline
+
+    files = ji(cmdline)
+    if not files:
+        return
+
+    gitroot = get_gitroot(".")
+
+    for src in files:
+        dirpart = os.path.join(outdir, os.path.dirname(src))
+        if not os.path.exists(dirpart):
+            os.makedirs(dirpart)
+        src = os.path.abspath(os.path.join(gitroot, src)).rstrip()
+        if os.path.exists(src) :
+            shutil.copy2(src, dirpart)
+            print "copied %s to %s" % (src, dirpart)
+
+def jichangelist(useless1, useless2) :
+    cmdline = "git status -s"
+    lines = ji(cmdline)
+    result = []
+    for line in lines:
+        if line.startswith("??") or line in result:
+            continue
+
+        result.append(line)
+
+    for line in result:
+        print line,
+
+def parse_cmdline(argv):
+    cmdline = None
+    fin = None
+    fout = None
+    while len(argv) > 0 :
+        arg = argv.pop(0).lower()
+        if arg == "-v":
+            global verbose
+            verbose = True
+        elif arg == "cat" :
+            cmdline = "cat"
+            fin = argv.pop(0)
+            fout = argv.pop(0)
+            break
+        elif arg == "backup" :
+            cmdline = "backup"
+            fin = argv
+            break
+        elif arg == "changelist" :
+            cmdline = "changelist"
+            break
+
+    return cmdline, fin, fout
+
+if __name__ == '__main__':
+    sys.argv.pop(0)
+    cmdfuncs = {
+            "cat"       : jicat,
+            "backup"    : jibackup,
+            "changelist": jichangelist,
+            }
+    cmd, fin, fout = parse_cmdline(sys.argv)
+    #print cmd, fin, fout
+
+    func = cmdfuncs.get(cmd, None)
+
+    if func:
+        func(fin, fout)
+    else:
+        print "Invalid command", __doc__
+
---
+++ vimrc
@@ -0,0 +1,142 @@
+set nocompatible
+
+if has('win32')
+    source $VIMRUNTIME/vimrc_example.vim
+    source $VIMRUNTIME/mswin.vim
+    behave mswin
+
+    map ,t :tabnew <C-R>=expand("%:p:h") . "\\" <CR>
+    map ,e :e <C-R>=expand("%:p:h") . "\\" <CR>
+elseif has("unix")
+    map ,t :tabnew <C-R>=expand("%:p:h") . "/" <CR>
+    map ,e :e <C-R>=expand("%:p:h") . "/" <CR>
+endif
+
+if &t_Co > 2 || has("gui_running")
+    syntax on
+    set hlsearch
+    set guioptions-=T
+    set isk+=-,?,_
+endif
+
+set spell
+
+set fileformats=unix,dos,mac
+
+set backspace=indent,eol,start
+
+set nobackup
+set writebackup
+
+" search related settings
+set history=100
+set ignorecase
+set smartcase
+set wrapscan
+set noincsearch
+
+" display related settings
+set title
+set showcmd
+set laststatus=2
+set showmatch
+set matchtime=2
+set wildmenu
+set wrap
+set number
+
+highlight LineNr ctermfg=darkyellow    
+highlight NonText ctermfg=darkgrey
+highlight Folded ctermfg=blue
+highlight SpecialKey cterm=underline ctermfg=darkgrey
+"highlight SpecialKey ctermfg=grey " 
+
+set smartindent
+set autoindent
+set iskeyword-=_
+set scrolloff=5
+
+"colorscheme murphy
+" tags settings
+"set tags=C:\Programe\\\ Files\vtag
+
+" complete type
+set wildmode=longest,full
+
+set statusline=[%L]\ %t\ %y%{'['.(&fenc!=''?&fenc:&enc).':'.&ff.']'}%r%m%=%c:%l/%L
+
+set guioptions-=T
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+" change window size
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+map <C-SPACE> :simalt ~x<CR>
+map <A-SPACE> :simalt ~r<CR>
+
+if has("autocmd")
+    filetype plugin indent on
+    autocmd BufEnter * lcd %:p:h "convert to current directory
+endif
+
+" Language Settings
+set fileencodings=utf-8
+
+"=======Tips=======
+"   Edit another file in the same directory as the current file
+"   uses expression to extract path from current file's path
+"  (thanks Douglas Potts)
+if has("unix")
+
+else
+endif
+
+set guitablabel=%{tabpagenr()}.%t\ %m
+
+" tab settings
+set ts=4 sw=4
+set softtabstop=4
+set shiftwidth=4
+set expandtab
+
+function LocalDiff2Depot()
+    on
+    diffthis
+    !p4 print -o %.depot %
+    vs
+    e %.depot
+    diffthis
+endfunction
+
+map ,d :call LocalDiff2Depot() <CR>
+
+function LocalDiffToSVN()
+    diffthis
+    !svn cat % > %.head
+    vs
+    e %.head
+    diffthis
+endfunction
+
+map ,s :call LocalDiffToSVN()<cr>
+
+function LocalDiffToGIT()
+    diffthis
+    !python <folder>/ji.py cat % %.head 
+    vs
+    e %.head
+    diffthis
+endfunction
+
+function LocalChangeList()
+    !python <folder>/ji.py changelist > changelist.tmp
+    e changelist.tmp
+endfunction
+
+map ,g :call LocalDiffToGIT()<cr>
+map ,l :call LocalChangeList()<cr>
+
+
+" cscope settings
+"cs add ~/cs/main/cscope.out
+" quickfix settings, use cwindow to show the search result
+" use h csqf to check the help
+set cscopequickfix=s-,g-,d-,c-,t-,e-,f-,i-
